08ae2c165818c04c2d68e8ffac1089277b261124
MariamLliso <contacto.mariamlliso@gmail.com>
2023-09-10 23:21:48 +0200

    :lipstick: Updated location of isologo and logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,9 @@
 import styles from './Logo.module.scss';
 
 const Logo = () => {
-  const isologo = '/images/elalifatoblog-isologo.svg';
+  const isologo = '/assets/images/elalifatoblog-isologo.svg';
   const isologoAlt = 'Isologo de elalifato.blog';
-  const logotipo = '/images/elalifatoblog-logotipo.svg';
+  const logotipo = '/assets/images/elalifatoblog-logotipo.svg';
   const logotipoAlt = 'Logotipo de elalifato.blog';
 
   return (
