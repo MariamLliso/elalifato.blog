d3291748a79a5c1dfcafd91ca4a31c6f8f87f9bb
MariamLliso <contacto.mariamlliso@gmail.com>
2023-09-11 00:14:41 +0200

    :sparkles: Rebrush Buttom component and update code

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,29 @@
+import { useRouter } from 'next/router';
 import styles from './Button.module.scss';
+import Icon from 'components/Icon/Icon';
 
-const Button = ({ children, className, ...rest }) => {
+const Button = ({ children, href, isCta, iconLeftName, iconRightName, ...rest }) => {
+  const router = useRouter();
   let buttonClassName = styles.button;
 
-  if (className) {
-    buttonClassName = `${buttonClassName} ${className}`;
+  if (isCta) {
+    buttonClassName = `${buttonClassName} ${styles.active}`;
   }
 
+  if (href && !isCta) {
+    buttonClassName = `${buttonClassName} ${router.asPath === href ? styles.active : ''}`;
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push(href);
+  };
+
   return (
-    <button {...rest} className={buttonClassName}>
+    <button type="button" onClick={handleClick} className={buttonClassName} {...rest}>
+      {iconLeftName && <Icon name={iconLeftName} />}
       {children}
+      {iconRightName && <Icon name={iconRightName} />}
     </button>
   );
 };
