b2135ed068d0008ed857b87e060a2cad40c950e7
MariamLliso <contacto.mariamlliso@gmail.com>
2023-09-11 00:14:06 +0200

    :lipstick: Update images & title at Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,13 @@
 import styles from './Logo.module.scss';
 
-const Logo = () => {
+const Logo = ({ title }) => {
   const isologo = '/assets/images/elalifatoblog-isologo.svg';
-  const isologoAlt = 'Isologo de elalifato.blog';
   const logotipo = '/assets/images/elalifatoblog-logotipo.svg';
-  const logotipoAlt = 'Logotipo de elalifato.blog';
 
   return (
     <div className={styles.logo}>
-      <img className={styles.logo__isologo} src={isologo} alt={isologoAlt} />
-      <img className={styles.logo__logotipo} src={logotipo} alt={logotipoAlt} />
+      <img className={styles.logo__isologo} src={isologo} alt={title} />
+      <img className={styles.logo__logotipo} src={logotipo} alt={title} />
     </div>
   );
 };
