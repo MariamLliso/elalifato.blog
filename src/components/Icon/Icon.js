f96cfb20a6a472e50f21c8eef6315b0f5ed351e1
MariamLliso <contacto.llisomartinez@gmail.com>
2023-09-11 12:23:05 +0200

    :lipstick: Added diferent sizes to icon component

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,6 +1,20 @@
-const Icon = ({ name, ...rest }) => {
+import styles from './Icon.module.scss';
+
+const Icon = ({ name, iconSize, ...rest }) => {
+  let iconClassName = `material-symbols-rounded`;
+
+  switch (iconSize) {
+    case 'large':
+      iconClassName = `${iconClassName} ${styles.icon__large}`;
+      break;
+
+    default:
+      iconClassName = `${iconClassName} ${styles.icon__regular}`;
+      break;
+  }
+
   return (
-    <span className="material-symbols-rounded" {...rest}>
+    <span className={iconClassName} {...rest}>
       {name}
     </span>
   );
