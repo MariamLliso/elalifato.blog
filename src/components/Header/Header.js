e33052d88aeefe16244b84e5ef29e6989e1a88e8
MariamLliso <contacto.llisomartinez@gmail.com>
2023-09-20 00:42:21 +0200

    :sparkles: Modify and style the common header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,27 @@
-import Container from 'components/Container';
-
 import styles from './Header.module.scss';
 
-const Header = ({ children }) => {
+const Header = ({ title, description, hasInnerHTML }) => {
   return (
     <header className={styles.header}>
-      <Container>{children}</Container>
+      <div className={styles.header__wrapper}>
+        {title && <h1 className={styles.header__title}>{title}</h1>}
+
+        {description && (
+          <>
+            <hr className={styles.header__divider} />
+            {hasInnerHTML ? (
+              <p
+                className={styles.header__description}
+                dangerouslySetInnerHTML={{
+                  __html: description,
+                }}
+              />
+            ) : (
+              <p className={styles.header__description}>{description}</p>
+            )}
+          </>
+        )}
+      </div>
     </header>
   );
 };
